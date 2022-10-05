b965928cd8d48ac1e91c2770fe5d8b9625394fb8
SylviaUzoh1 <112405036+SylviaUzoh1@users.noreply.github.com>
2022-10-05 13:51:55 +0100

    Add files via upload

---
+++ No.5.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract tech4dev{
+
+    // Create a function that consist of if and else statement, and give an example with it.
+    function conditional(uint _y) public pure returns(uint){
+        if(_y <=5){
+        return 6;
+
+        } else{
+        return 1;
+        }
+    }
+
+}
