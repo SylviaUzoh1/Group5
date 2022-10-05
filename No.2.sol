15f2e95b60a1173f28d9ffffafc513e514f35d62
SylviaUzoh1 <112405036+SylviaUzoh1@users.noreply.github.com>
2022-10-05 13:50:04 +0100

    Add files via upload

---
+++ No.2.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract group5{
+
+//Give 2 examples of fixed array
+
+   uint[5] age= [1,2,3,4,5];
+   string[2] name= ["Reham", "Sylvia"];
+
+   //Dynamic Array
+
+   uint[] height;
+   string[] class;
+
+}
